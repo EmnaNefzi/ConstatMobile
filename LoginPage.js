f91975a41690fa64b448fb45bafcdbecd14563c9
EmnaNefzi <152291766+EmnaNefzi@users.noreply.github.com>
2024-05-06 20:47:13 +0100

    Update LoginPage.js

--- LoginPage.js
+++ LoginPage.js
@@ -1,54 +1,42 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet ,onPress} from 'react-native';
+import { View, TextInput, Button, Alert , Image,Text} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import App from '../App';
-import HomePage from './HomePage';
-import VehiculeA from './VehiculeA';
 
 export default function LoginPage() {
   const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-
-
-    const goToVehiculeA = () => {
+  const goToVehiculeA = () => {
+    if (email.trim() === '' || password.trim() === '') {
+      // Si l'email ou le mot de passe est vide, affichez une alerte
+      Alert.alert('Erreur', 'Veuillez remplir tous les champs.');
+    } else {
+      // Sinon, naviguez vers la page VehiculeA
       navigation.navigate('VehiculeA');
-    };
-
+    }
+  };
 
   return (
-    <View style={{    flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image source={require('./cm.png')} style={{ height: 150, width: 150, top: -30 }} />
+      <Text style={{  fontWeight: 'bold', marginBottom:5, marginHorizontal: 10, right:115}}>Adresse E-mail :</Text>
       <TextInput
-        style={{    width: '80%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: 'gray',
-        marginBottom: 20,
-        paddingHorizontal: 10}}
+        style={{ width: '80%', height: 40, borderWidth: 2, borderColor: 'gray', marginBottom: 20, paddingHorizontal: 10 }}
         placeholder="Votre Email"
         keyboardType="email-address"
         value={email}
         onChangeText={(text) => setEmail(text)}
       />
+      <Text style={{  fontWeight: 'bold', marginBottom:5, marginHorizontal: 10, right:115}}>Mot De Passe :</Text>
       <TextInput
-        style={{    width: '80%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: 'gray',
-        marginBottom: 20,
-        paddingHorizontal: 10}}
+        style={{ width: '80%', height: 40, borderWidth:2, borderColor: 'gray', marginBottom: 20, paddingHorizontal: 10 }}
         placeholder="Votre Mot De Passe"
         secureTextEntry
         value={password}
         onChangeText={(text) => setPassword(text)}
       />
-      <Button title="Connecter" onPress={goToVehiculeA} />
+      <Button title="Connecter" onPress={goToVehiculeA}  color="#5239A0" />
     </View>
   );
-};
-
-
-
+}
