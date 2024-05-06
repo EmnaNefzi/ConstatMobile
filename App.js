fcf24473975b34001cc1b0a8dba582a5bb14c4b2
EmnaNefzi <152291766+EmnaNefzi@users.noreply.github.com>
2024-05-06 20:39:33 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
+// App.js (ou votre fichier principal de navigation)
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'; 
+import { createStackNavigator } from '@react-navigation/stack';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import VehiculeA from './components/VehiculeA';
-
+import VehiculeA2 from './components/VehiculeA2';
+import CodeGR from './components/CodeGR';
+import CarDamageForm from './components/CarDamageForm';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Bienvenue" component={HomePage} />
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Signup" component={SignupPage} />
-        <Stack.Screen name="vehiculeA" component={VehiculeA}/>
+      <Stack.Navigator initialRouteName="HomePage">
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="SignupPage" component={SignupPage} />
+        <Stack.Screen name="VehiculeA" component={VehiculeA} />
+        <Stack.Screen name="VehiculeA2" component={VehiculeA2} />
+        <Stack.Screen name="Code GR" component={CodeGR} />
+        <Stack.Screen name="CarDamageForm" component={CarDamageForm} />
       </Stack.Navigator>
     </NavigationContainer>
   );
